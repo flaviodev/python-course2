72656ba7f981ad5db77ae2e5ef146b29f01e57ee
Flávio de Souza <fdsdev@gmail.com>
2018-12-06 01:54:19 -0200

    loading mongo

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 app_path=`pwd`
 
 docker rm flaviodev-python -f
-docker run -it --name flaviodev-python -v $app_path/src:/usr/src:rw flaviodev-python bash
+docker run -it --name flaviodev-python -v $app_path/src:/usr/src:rw -v $app_path/data:/data:rw flaviodev-python sh
