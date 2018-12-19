3631934ccc68b7eb1821a14f4fbcc3dbd6ff00c8
Flávio de Souza <fdsdev@gmail.com>
2018-12-18 23:43:35 -0200

    improving encapsulation

--- src/models/profile.py
+++ src/models/profile.py
@@ -1,15 +1,28 @@
 from mongoengine import *
 
 class Profile(Document):
-    name = StringField(required=True, max_length=200)
-    __likes = LongField(default=0)
+    __name = StringField(name='name', required=True, max_length=200)
+    __likes = LongField(name='likes', default=0)
+    __vip = BooleanField(name = 'vip', default=False)
+
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, name):
+        self.__name = name
 
     def get_likes(self):
         return self.__likes
+
+    def is_vip(self):
+        return self.__vip
+
+    def set_vip(self, vip):
+        self.__vip = vip
 
     def like(self):
         self.__likes += 1
         self.save()
 
     def get_credits(self):
-        return self.__likes * 10.0
+        return self.__likes * 10.0 if self.__vip else 0.0
