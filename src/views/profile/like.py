64b2b5afee1b7439cfc5e5a53e9cdfd45b97dbc2
Flávio de Souza <fdsdev@gmail.com>
2018-12-18 23:45:01 -0200

    including like feature and credits

--- src/views/profile/like.py
+++ src/views/profile/like.py
@@ -10,14 +10,14 @@ def run():
 
     i = 1
     for profile in Profile.objects:
-        print('{} - {}'.format(i, profile.name))
+        print('{} - {}'.format(i, profile.get_name()))
         i += 1
 
     try:
         profile_id = int(input("Type the profile id you want to like: "))
         profile = Profile.objects[profile_id-1]
 
-        print('Liking profile: {} - {}'.format(profile_id, profile.name))
+        print('Liking profile: {} - {}'.format(profile_id, profile.get_name()))
         profile.like()
     except:
         print('Type a valid profile id!')
